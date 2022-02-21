bc532d38fcdbe8e447cfb109d9a1514578d91a34
Aywish <irishrivera0207@gmail.com>
2022-02-21 13:28:28 +0800

    added image upload

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -12,6 +12,55 @@ firebase.auth().onAuthStateChanged(function(custUser) {
   }
 });
 
+
+
+firebase.database()
+.ref("adminProducts/" + 1)
+.on("value", function (snap) {
+  var showHeadline = snap.val().prodHeadline;
+  var showDesc = snap.val().shortDescription;
+  var link = snap.val().prodLink;
+
+  document.getElementById('image1').src = snap.val().imageLink;
+  document.getElementById('headline1').innerHTML = showHeadline;
+  document.getElementById('desc1').innerHTML = showDesc;
+  document.getElementById('link1').href = link;
+});
+
+firebase.database()
+.ref("adminProducts/" + 2)
+.on("value", function (snap) {
+  var showHeadline = snap.val().prodHeadline;
+  var showDesc = snap.val().shortDescription;
+
+  document.getElementById('image2').src = snap.val().imageLink;
+  document.getElementById('headline2').innerHTML = showHeadline;
+  document.getElementById('desc2').innerHTML = showDesc;
+});
+
+firebase.database()
+.ref("adminProducts/" + 3)
+.on("value", function (snap) {
+  var showHeadline = snap.val().prodHeadline;
+  var showDesc = snap.val().shortDescription;
+
+  document.getElementById('image3').src = snap.val().imageLink;
+  document.getElementById('headline3').innerHTML = showHeadline;
+  document.getElementById('desc3').innerHTML = showDesc;
+});
+
+firebase.database()
+.ref("adminProducts/" + 4)
+.on("value", function (snap) {
+  var showHeadline = snap.val().prodHeadline;
+  var showDesc = snap.val().shortDescription;
+
+  document.getElementById('image4').src = snap.val().imageLink;
+  document.getElementById('headline4').innerHTML = showHeadline;
+  document.getElementById('desc4').innerHTML = showDesc;
+});
+
+
 orderCustom.onclick = function () {
   warning = "Order Custom is only available for wholesale orders with no less than 300 pair of shoes. \n\nWould you like to proceed?";
 
